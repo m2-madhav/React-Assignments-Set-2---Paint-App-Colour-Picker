d0e626e5d656259333e804dfe8da439b56f30cc1
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-12-04 18:12:39 +0530

    code-added

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,15 @@
+import React from "react";
 
+export default function Selection(props) {
+  const { applyColor } = props;
+  const [colourChange, setcolourChange] = React.useState({ background: "" });
+  return (
+    <div
+      className="fix-box"
+      style={colourChange}
+      onClick={() => applyColor(setcolourChange)}
+    >
+      <h2 class="subheading">Selection</h2>
+    </div>
+  );
+}
